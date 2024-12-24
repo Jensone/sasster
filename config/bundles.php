636b402970a096cd764ca555355d4113d1430aba
Jensone <hello@jensone.com>
2024-12-24 15:45:27 +0100

    Update .gitignore, add Doctrine Fixtures Bundle for dev/test, and add cover image for user profiles in public folder

--- config/bundles.php
+++ config/bundles.php
@@ -24,4 +24,5 @@ return [
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
     Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
